4ea6daa92826077aafafca504373749cd2ec1a42
chisl <info@chisl.io>
2018-01-11 12:09:53 +0100

    Initial commit

---
+++ IIS2MDC_constants.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""IIS2MDC: High-accuracy, ultra-low-power, 3-axis digital output magnetometer"""
+
+__author__     = "ChISL"
+__copyright__  = "TBD"
+__credits__    = ["STMicroelectronics"]
+__license__    = "TBD"
+__version__    = "Version 0.1"
+__maintainer__ = "https://chisl.io"
+__email__      = "info@chisl.io"
+__status__     = "Test"
+
+#
+#   THIS FILE IS AUTOMATICALLY CREATED
+#    D O     N O T     M O D I F Y  !
+#
+
+class REG:
+	OFFSET_X = 69
+	OFFSET_Y = 71
+	OFFSET_Z_ = 73
+	WHO_AM_I = 79
+	CFG_REG_A = 96
+	CFG_REG_B = 97
+	CFG_REG_C = 98
+	INT_CTRL_REG = 99
+	INT_SOURCE_REG = 100
+	INT_THS_L_REG = 101
+	STATUS_REG = 103
+	OUTX = 104
+	OUTY = 106
+	OUTZ = 108
